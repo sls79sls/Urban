c2770897d27079e4b483238d293278e5a4c76481
sls79sls <sls_79@mail.ru>
2024-09-20 10:30:00 +0300

    Create fake_math.py

---
+++ fake_math.py
@@ -0,0 +1,9 @@
+def divide(first_, second_):
+    if (second_ == 0):
+        return (f'{first_} / {second_} = Делитель равен нулю. Ошибка')
+    else:
+        return (f'{first_} / {second_} = {first_ / second_}')
+
+
+if __name__ == "__main__":
+   pass
