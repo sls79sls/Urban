1d933bfb025edc8a62746e2720f4bac80e686d3b
sls79sls <sls_79@mail.ru>
2024-10-09 20:54:05 +0300

    Update Module_6_1.py

--- Module_6_1.py
+++ Module_6_1.py
@@ -1,35 +1,51 @@
 class Animal:
-    def __init__(self, name, alive = True, fed = False):
+    def __init__(self, name, alive=True, fed=False):
         self.name = name
+        self.alive = alive
+        self.fed = fed
+
 
 class Mammal(Animal):
     def eat(self, food):
-        
+        if self.food.edible:
+            print (f'{self.name} съел {food.name}')
+            self.fed = True
+
 
 class Predator(Animal):
     def eat(self, food):
+        if self.food.edible:
+            print (f'{self.name} съел {food.name}')
+            self.fed = True
+
 
 class Plant:
-    def __init__(self,name, edible = False):
+    def __init__(self, name, edible=False):
+        self.name = name
+        self.edible = edible
+
 
 class Flower(Plant):
-    def __init__(self, edible=False):
+    def __init__(self, edible=True):
+        self.edible = edible
+
 
 class Fruit(Plant):
     def __init__(self, edible=True):
+        self.edible = edible
 
 
-        a1 = Predator('Волк с Уолл-Стрит')
-        a2 = Mammal('Хатико')
-        p1 = Flower('Цветик семицветик')
-        p2 = Fruit('Заводной апельсин')
+    a1 = Predator('Волк с Уолл-Стрит')
+    a2 = Mammal('Хатико')
+    p1 = Flower('Цветик семицветик')
+    p2 = Fruit('Заводной апельсин')
 
-        print(a1.name)
-        print(p1.name)
+    print(a1.name)
+    print(p1.name)
 
-        print(a1.alive)
-        print(a2.fed)
-        a1.eat(p1)
-        a2.eat(p2)
-        print(a1.alive)
-        print(a2.fed)
+    print(a1.alive)
+    print(a2.fed)
+    a1.eat(p1)
+    a2.eat(p2)
+    print(a1.alive)
+    print(a2.fed)
