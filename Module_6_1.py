63284be577ae76da45dd9b9fb37ef9bf71049a34
sls79sls <sls_79@mail.ru>
2024-10-09 21:29:14 +0300

    Update Module_6_1.py

--- Module_6_1.py
+++ Module_6_1.py
@@ -4,19 +4,17 @@ class Animal:
         self.alive = alive
         self.fed = fed
 
+    def eat(self, food):
+        if self.food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+
 
 class Mammal(Animal):
-    def eat(self, food):
-        if self.food.edible:
-            print (f'{self.name} съел {food.name}')
-            self.fed = True
-
+    pass
 
 class Predator(Animal):
-    def eat(self, food):
-        if self.food.edible:
-            print (f'{self.name} съел {food.name}')
-            self.fed = True
+    pass
 
 
 class Plant:
@@ -35,17 +33,17 @@ class Fruit(Plant):
         self.edible = edible
 
 
-    a1 = Predator('Волк с Уолл-Стрит')
-    a2 = Mammal('Хатико')
-    p1 = Flower('Цветик семицветик')
-    p2 = Fruit('Заводной апельсин')
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
 
-    print(a1.name)
-    print(p1.name)
+print(a1.name)
+print(p1.name)
 
-    print(a1.alive)
-    print(a2.fed)
-    a1.eat(p1)
-    a2.eat(p2)
-    print(a1.alive)
-    print(a2.fed)
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
