c6daeadf75a6ad3fa11f90d2bbef3926e5d693a9
sls79sls <sls_79@mail.ru>
2024-11-03 16:16:29 +0300

    Update Module_9_4.py

--- Module_9_4.py
+++ Module_9_4.py
@@ -1,11 +1,19 @@
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
-print(type(first))
-print(list(first))
-print(type(first))
-for i in range(len(second)):
-    print (f'i = {i}')
-    print(f'range(len(second) = {range(len(second))}')
-    print(f'first[{i}] = {first[i]}')
+print(len(first))
+print(len(second))
+result = []
+first_list = list(first)
+second_list = list(second)
+for i in range(len(first),len(second)-1):
     print(f'second[{i}] = {second[i]}')
-    list(map(lambda x, y: list(x[i]) == list(y[i]), first, second))
+    first_list[i].append('%')
+    print(f'second[{i}] = {second[i]}')
+    print(f'first[{i}] = {first[i]}')
+for i in range(len(second)):
+        print (f'i = {i}')
+        print(f'range(len(second) = {range(len(second))}')
+        print(f'first[{i}] = {first[i]}')
+        print(f'second[{i}] = {second[i]}')
+        result = list(map(lambda x, y: x[i] == y[i], first_list, second_list))
+
