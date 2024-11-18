af0135de8b2a6b69273fccfa4a1fddfb975d1e86
sls79sls <sls_79@mail.ru>
2024-11-18 17:02:54 +0300

    Update Module_10_4.py

--- Module_10_4.py
+++ Module_10_4.py
@@ -17,31 +17,26 @@ class Guest(threading.Thread):
     def run(self):
         self.waiting_ = randint(0, 2)
         time.sleep(self.waiting_)
-        return self.waiting_
 
-class Cafe(Table, Guest):
-    global guests
+class Cafe:
     def __init__(self,*tables):
-        threading.Thread.__init__(self)
-        self.guests = guests
-    q = Queue()
-    q.put(self.guests)
-    print (f'q.get(guests) = {q.get(guests)}')
-    def guest_arrival(self,*guests):
-        global count_
-        print(f'len(list(*guests)) : {len(list(*guests))}')
-        print(f'len(list(tables)) : {len(list(tables))}')
-        while (count_ <= len(list(*guests))):
-            for i in range (1,len(list(tables))):
-                print(f'i =  : {i}')
-                if (tables[i].guest == None):
-                    tables[i].guest = guests[count_]
-                    thread = Guest('thread Guests')
-                    thread.start()
-                    print(f'Гость, {self.guests[count_]}, сел за стол {i}')
-                continue
-            count_ += 1
-            print(f'self.guests[count_] : {self.guests[count_]}')
+        self.tables = tables
+        self.q = Queue()
+    def guest_arrival(self, *guests):
+        for guest in guests:
+            free_table = None
+            for table in self.tables:
+                if table.guest == None:
+                    free_table = table
+                    break
+            if free_table:
+                free_table.guest = guest
+                guest.start()
+                print(f"{guest.name} сел(-а) за стол номер {free_table.number}")
+            else:
+                self.q.put(guest)
+                print(f"{guest.name} в очереди")
+
     def discuss_guests(self):
         pass
 if __name__ == '__main__':
