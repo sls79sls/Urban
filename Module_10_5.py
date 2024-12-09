1ff8fe4a5295d80121b75d37be89820f934f09a8
sls79sls <sls_79@mail.ru>
2024-12-09 19:10:46 +0300

    Update Module_10_5.py

--- Module_10_5.py
+++ Module_10_5.py
@@ -1,27 +1,30 @@
 import multiprocessing as mp
+from multiprocessing import Pool
 import datetime
+
 
 def read_info(name):
     all_data = []
-    file = open(name, "r")
-    while True:
-        line = file.readline()
-        all_data.append(line)
-        if not line:
-            break
-        #print(line.strip())
-    file.close
-    return (all_data)
+    with open(name, "r") as file:
+        while True:
+            line = file.readline()
+            all_data.append(line.strip())
+            if not line:
+                break
+    return all_data
+
 
 if __name__ == '__main__':
     filenames = [f'file {number}.txt' for number in range(1, 5)]
+
     start = datetime.datetime.now()
-    read_info(filenames[0])
-    read_info(filenames[1])
-    read_info(filenames[2])
-    read_info(filenames[3])
+    for file in filenames:
+        read_info(file)
     end = datetime.datetime.now()
     print(f'Monoprocessing : {end - start} секунд')
 
-    pool = Pool([4, read_info(), filenames[0], maxtasksperchild
-                 [, context])
+    start_pool = datetime.datetime.now()
+    with Pool(3) as pool:
+        pool.map(read_info, filenames)
+    end_pool = datetime.datetime.now()
+    print(f'Multiprocessing : {end_pool - start_pool} секунд')
