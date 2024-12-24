45edc636522288b944ea833a247e22b8193be788
sls79sls <sls_79@mail.ru>
2024-12-24 20:28:56 +0300

    Update Introspection.py

--- Introspection.py
+++ Introspection.py
@@ -6,13 +6,18 @@ import inspect
 
 
 def introspection_info(obj):
+    dict_intro["Type"] = type(obj)
+    dict_intro["Names"] = requests.__name__
+    dict_intro["Attribute"] = dir(requests)
+    dict_intro["Module"] = inspect.ismodule(obj)
+    dict_intro["List_arg"] = sys.argv
 
-    return type(obj), requests.__name__, dir(requests), inspect.ismodule(obj), sys.argv
-
+    return dict_intro
 
 if __name__ == '__main__':
-
+    dict_intro = {}
     number_info = introspection_info(requests)
-    for i in number_info:
-        print(i)
-        #print(number_info)
+    print (dict_intro)
+    # for i in number_info:
+    #     print(i)
+    #     #print(number_info)
