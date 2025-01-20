d1cbee7ed7d09e5997987745a46e2a90cd436f03
sls79sls <sls_79@mail.ru>
2025-01-20 15:40:52 +0300

    Update Test_12_2.py

--- Test_12_2.py
+++ Test_12_2.py
@@ -5,9 +5,9 @@ class TournamentTest(unittest.TestCase):
     print("Start")
     @classmethod
     def setUpClass(cls):
-        all_results = {}
+        cls.all_results =[]
         print("НАЧАЛО ТЕСТА")
-        return all_results
+        return cls.all_results
 
     def setUp(self):
         super(TournamentTest,self).setUp()
@@ -20,28 +20,42 @@ class TournamentTest(unittest.TestCase):
 
     print("Тест-1")
     def test_Tour0(self):
-        self.T0 = Runners.Tournament(self, (self.human1, self.human3), 90)
-        result = self.T0.start()
-        print(f'result 1-stfunction = {result}')
-        #self.assertTrue((list(result.values())[-1]), self.human3.name, 'НЕ совпадает')
-        print(f'all_results = {result}')
+        T0 = Runners.Tournament(90, self.human1, self.human3)
+        s=T0.start()
+        print(f' s = {s}')
+        print(f'Тест-1 : Begin - T0-all_results = {self.all_results}')
+        self.all_results.append(s)
+        print(f'Тест-1 : Middle - T0-all_results = {self.all_results}')
+        #print(f'self.all_results[0][min(s))] = {self.all_results[0][min(s)]}')
+        self.assertTrue(self.all_results[0][max(s)] == 'Ник')
+        print(f'Тест-1 : Last - T0-all_results = {self.all_results}')
 
     print("Тест-2")
-    def Tour1(self):
-        self.T1 = Runners.Tournament(self, (self.human2, self.human3), 90)
-        self.assertTrue((list(self.T1.start().values())[-1]), self.human3.name, 'НЕ совпадает')
-        print(f'all_results = {self.all_results}')
+    def test_Tour1(self):
+        T1 = Runners.Tournament(90, self.human2, self.human3)
+        s = T1.start()
+        print(f' s = {s}')
+        print(f'Тест-2 : Begin - T1-all_results = {self.all_results}')
+        self.all_results.append(s)
+        print(f'Тест-2 : Middle - T1-all_results = {self.all_results}')
+        self.assertTrue(self.all_results[1][max(s)] == 'Ник')
+        print(f'Тест-2 : Last - T1-all_results = {self.all_results}')
 
     print("Тест-3")
-    def Tour2(self):
-        self.T2 = Runners.Tournament(self, (self.human1, self.human2, self.human3), 90)
-        self.assertTrue(list(self.T2.start().values())[-1], 'НЕ совпадает')
-        print(f'all_results = {self.all_results}')
+    def test_Tour2(self):
+        T2 = Runners.Tournament(90, self.human1, self.human2, self.human3)
+        s = T2.start()
+        print(f' s = {s}')
+        print(f'Тест-3 : Begin - T2-all_results = {self.all_results}')
+        self.all_results.append(s)
+        print(f'Тест-3 : Middle - T2-all_results = {self.all_results}')
+        #self.assertTrue(self.all_results[2][max(s)] == 'Ник')
+        print(f'Тест-3 : Last - T2-all_results = {self.all_results}')
 
     @classmethod
     def tearDownClass(cls):
-
-        print (f'#all_results = {None}')
+        for i in cls.all_results:
+            print ('{ 1 : ', i[1] , ', 2 : ' , i[2] , '}')
 
 
 if __name__ == '__main__':
