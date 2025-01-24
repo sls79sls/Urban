c3f025501694e07bbdc533d90e5bd3bcc3d7a7a8
sls79sls <sls_79@mail.ru>
2025-01-24 15:57:24 +0300

    Create RunnerTest.py

---
+++ RunnerTest.py
@@ -0,0 +1,29 @@
+import Runer
+import unittest
+
+class RunerTest(unittest.TestCase):
+    is_frozen = False
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_walk(self):
+        human = Runer.Runner('LSS')
+        for i in range(10):
+            human.walk()
+        self.assertEqual(human.distance,50, 'Walk : Ошибка')
+
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_run(self):
+        human = Runer.Runner('SLS')
+        for i in range(10):
+            human.run()
+        self.assertEqual(human.distance, 100, 'Run : Ошибка')
+    @unittest.skipIf(is_frozen, "Тесты в этом кейсе заморожены")
+    def test_challenge(self):
+        human1 = Runer.Runner('Fed')
+        human2 = Runer.Runner('Ded')
+        for i in range(10):
+            human1.run()
+            human2.walk()
+        self.assertNotEqual(human1.distance, human2.distance, 'Run&Walk NotEqual : Ошибка')
+
+if __name__ == '__main__':
+    unittest.main()
